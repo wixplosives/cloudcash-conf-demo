d2aad5db54c0dcc132cc7146a363bd14def560c3
Ari Kishon <ariki@wix.com>
2023-03-28 16:36:22 +0300

    Initial commit

---
+++ src/_codux/boards/goals/goals.board.tsx
@@ -0,0 +1,10 @@
+import { createBoard } from '@wixc3/react-board';
+import { Goals } from '../../../components/goals/goals';
+
+export default createBoard({
+    name: '🧩 Goals Section',
+    Board: () => <Goals />,
+    environmentProps: {
+        canvasWidth: 874,
+    },
+});
---
+++ src/_codux/boards/outcome-statistics/outcome-statistics.board.tsx
@@ -0,0 +1,10 @@
+import { createBoard } from '@wixc3/react-board';
+import { OutcomeStatistics } from '../../../components/outcome-statistics/outcome-statistics';
+
+export default createBoard({
+    name: '🧩 Outcome Statistics Section',
+    Board: () => <OutcomeStatistics />,
+    environmentProps: {
+        canvasWidth: 874,
+    },
+});
